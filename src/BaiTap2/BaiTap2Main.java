ce21c4ca3db995844fbabb5a2ceec60f947e5e7a
Administrator <nhacpham550212@gmail.com>
2020-04-26 14:41:55 +0700

    StrateryPattern

--- src/BaiTap2/BaiTap2Main.java
+++ src/BaiTap2/BaiTap2Main.java
@@ -16,23 +16,27 @@ public class BaiTap2Main {
      */
     public static void main(String[] args) {
         // TODO code application logic here
-        HangHoa hanghoa1 =new HangHoa("Xe đạp ", 1200000, "xe đạp mã mới nhất");
-        HangHoa hanghoa2=new HangHoa("Xe Máy", 22000000, "Xe Máy HonDa đời mới");
-        HangHoa hanghoa3=new HangHoa("Xe Ô Tô", 200000000, "xe ô tô rẻ");
+        HangHoa hanghoa1 =new HangHoa("Xe đạp ", 500000, "xe đạp mã mới nhất");
+        HangHoa hanghoa2=new HangHoa("Xe Máy", 5000000, "Xe Máy HonDa đời mới");
+        HangHoa hanghoa3=new HangHoa("Tủ lạnh ", 4000000, "tủ lạnh đứng 2 tầng");
         HangHoa hanghoa4=new HangHoa("Bánh xe dự phòng ô tô", 1500000, "bánh xe lốp dày");
         //
         GioHang giohang1 = new GioHang();
+        GioHang giohang2 = new GioHang();
+        //
         giohang1.themHangHoa(hanghoa1);
         giohang1.themHangHoa(hanghoa2);
+        System.out.println("Danh sách Hàng Hóa giỏ hàng 1:\n");
         giohang1.inDanhSach();
         giohang1.setHinhThucTT(new ThanhToanOnline());
-        System.out.println("Tiền khách hàng phải trả là :"+giohang1.thanhToan(12000000));
+        System.out.println("Tiền khách hàng phải trả là :"+giohang1.thanhToan(giohang1.getGiatienHang())+"\n");
         //
-        GioHang giohang2 = new GioHang();
         giohang2.themHangHoa(hanghoa3);
         giohang2.themHangHoa(hanghoa4);
+        System.out.println("Danh sách Hàng Hóa giỏ hàng 2:\n");
         giohang2.inDanhSach();
         giohang2.setHinhThucTT(new ThanhToanCOD());
+        System.out.println("Tiền khách hàng phải trả là :"+giohang1.thanhToan(giohang2.getGiatienHang())+"\n");
     }
     
 }
